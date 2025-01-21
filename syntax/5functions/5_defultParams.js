f5d73e1af9d1eeffad98ea29eb0a922d777b8dbf
silvesterjk <silvesterjkennedy@gmail.com>
2025-01-21 15:33:02 +0530

    Create 5_defultParams.js

---
+++ syntax/5functions/5_defultParams.js
@@ -0,0 +1,8 @@
+// When no parameter is passed, the default value is used. The default value is undefined.
+
+function writeCode(language) {
+    console.log(`write code in ${language}`);
+}
+
+writeCode('JavaScript'); // write code in JavaScript
+writeCode(); // write code in undefined
