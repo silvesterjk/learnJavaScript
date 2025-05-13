df895911a5c9f2cfc5230bcae875eefd1103a77a
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-05-13 20:20:09 +0530

    update

--- projects/two/src/App.js
+++ projects/two/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello there! Silvester here.
         </p>
         <a
           className="App-link"
