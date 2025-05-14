dbd89d18e836b126af9aab2aa7b96cea977f8652
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-05-14 22:09:55 +0530

    Update starter.js

--- react/02-js/starter.js
+++ react/02-js/starter.js
@@ -177,7 +177,7 @@ const newBook = {
     ...book,
 
     // Adding a new property to the object
-    
+
     moviePublicationDate: '2023-01-01', 
 
     // Overriding an existing property
@@ -186,3 +186,5 @@ const newBook = {
 
 console.log(newBook);
 // { id: 2, title: 'The Cyberiad', publicationDate: '1965-01-01', author: 'Stanislaw Lem', genres: [ 'science fiction', 'humor', 'speculative fiction', 'short stories', 'fantasy' ], hasMovieAdaptation: false, pages: 295, translations: {}, reviews: { goodreads: { rating: 4.16, ratingsCount: 11663, reviewsCount: 812 }, librarything: { rating: 4.13, ratingsCount: 2434, reviewsCount: 0 } }, moviePublicationDate: '2023-01-01', pages: 499 }
+
+// The spread operator can also be used to spread an array into individual elements.
