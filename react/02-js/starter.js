8086524f4194f7e328c69259af98222acd4a0e27
Silvester J Kennedy <108580657+silvesterjk@users.noreply.github.com>
2025-05-14 22:10:17 +0530

    Update starter.js

--- react/02-js/starter.js
+++ react/02-js/starter.js
@@ -187,4 +187,8 @@ const newBook = {
 console.log(newBook);
 // { id: 2, title: 'The Cyberiad', publicationDate: '1965-01-01', author: 'Stanislaw Lem', genres: [ 'science fiction', 'humor', 'speculative fiction', 'short stories', 'fantasy' ], hasMovieAdaptation: false, pages: 295, translations: {}, reviews: { goodreads: { rating: 4.16, ratingsCount: 11663, reviewsCount: 812 }, librarything: { rating: 4.13, ratingsCount: 2434, reviewsCount: 0 } }, moviePublicationDate: '2023-01-01', pages: 499 }
 
-// The spread operator can also be used to spread an array into individual elements.
+// The spread operator can also be used to spread an array into individual elements
+const newAuthor = 'Stanislaw Lem';
+const newGenres = [...genres, 'epic fantasy'];
+console.log(newGenres); // science fiction humor epic fantasy
+// [ 'science fiction', 'humor', 'speculative fiction', 'short stories', 'fantasy', 'epic fantasy' ]
